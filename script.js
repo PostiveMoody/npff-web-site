c14740b231d21b2c528e78faff579b1c712dad39
Викторов Павел Валерьянович <decidedtosolvethisproblem@gmail.com>
2025-06-15 16:12:20 +0300

    MVP

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.header-container-staff').addEventListener('click',()=>{
+    window.open('about.html','_blank')
+})
+
+document.querySelector('.main-container-button').addEventListener('click', ()=>{
+    window.open('https://clck.ru/3McmV8', '_blank')
+})
